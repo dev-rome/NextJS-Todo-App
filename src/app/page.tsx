5a15d22963cac389ed65b7274a2e7f36c82f1e27
dev-rome <haynesjerome@gmail.com>
2023-05-24 15:20:17 -0400

    added add task and map over todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
+import { getTodos } from "@/app/api/route";
+import AddTask from "@/components/AddTask";
 import TodoList from "@/components/TodoList";
-import { AiOutlinePlus } from "react-icons/ai";
 
-export default function Home() {
+export default async function Home() {
+  const todo = await getTodos();
+
   return (
     <main className="max-w-4xl mx-auto h-screen mt-4 px-4">
-      <div className="flex flex-col items-start md:flex-row md:justify-between md:items-center">
+      <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl font-medium underline">Todo List App</h1>
-        <button className="flex items-center gap-2 bg-[#339af0] text-[#f8f9fa] rounded mt-3 md:mt-0 py-2 px-4">
-          Add New Task <AiOutlinePlus size={18} />
-        </button>
+        <AddTask />
       </div>
       <div className="flex justify-between items-center bg-[#ced4da] rounded mt-3 py-3 px-4">
         <p className="text-xs font-bold">TASK</p>
         <p className="text-xs font-bold">ACTION</p>
       </div>
-      {/* <TodoList /> */}
+      <ul className="flex flex-col gap-3 mt-3">
+        {todo.map((item) => (
+          <TodoList key={item.id} item={item} />
+        ))}
+      </ul>
     </main>
   );
 }
