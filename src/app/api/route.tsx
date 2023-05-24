17e7e126293f0286d6fbe852c4abaa2d1a43cabf
dev-rome <haynesjerome@gmail.com>
2023-05-24 16:41:41 -0400

    change to add todo item, added edit and delete functions

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -11,7 +11,7 @@ export const getTodos = async (): Promise<TodoProps[]> => {
   return todos;
 };
 
-export const addTodo = async (text: TodoProps): Promise<TodoProps> => {
+export const addTodoItem = async (text: TodoProps): Promise<TodoProps> => {
   const res = await fetch("http://localhost:3001/todo-list", {
     method: "POST",
     body: JSON.stringify(text),
@@ -21,4 +21,24 @@ export const addTodo = async (text: TodoProps): Promise<TodoProps> => {
   });
   const todo = await res.json();
   return todo;
+};
+
+export const editTodoItem = async (text: TodoProps): Promise<TodoProps> => {
+  const res = await fetch(`http://localhost:3001/todo-list/${text.id}`, {
+    method: "PUT",
+    body: JSON.stringify(text),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const updateTodo = await res.json();
+  return updateTodo;
+};
+
+export const deleteTodoItem = async (id: string): Promise<TodoProps> => {
+  const res = await fetch(`http://localhost:3001/todo-list/${id}`, {
+    method: "DELETE",
+  });
+  const deleteTodo = await res.json();
+  return deleteTodo;
 };
