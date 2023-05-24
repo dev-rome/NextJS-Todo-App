3b5ce9bcbaeafa22b24a9e6d0ef8e3f20084a72f
dev-rome <haynesjerome@gmail.com>
2023-05-24 16:44:25 -0400

    edit modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineClose } from "react-icons/ai";
 
 interface ModalProps {
-  onClick: () => void;
+  onClick?: () => void;
   children?: React.ReactNode;
 }
 
@@ -13,9 +13,15 @@ const Modal = ({ onClick, children }: ModalProps) => {
       onClick={onClick}
       className="fixed inset-0 bg-[#212529] bg-opacity-25 backdrop-blur-sm flex items-center justify-center z-10 px-4"
     >
-      <div onClick={stopPropagation} className="bg-[#f8f9fa] rounded max-w-md w-full flex justify-between p-4">
+      <div
+        onClick={stopPropagation}
+        className="bg-[#f8f9fa] rounded max-w-md w-full flex justify-between p-4"
+      >
         {children}
-        <button onClick={onClick} className="bg-[#339af0] rounded-full p-1 self-start">
+        <button
+          onClick={onClick}
+          className="bg-[#339af0] rounded-full p-1 self-start"
+        >
           <AiOutlineClose size={16} color="#f8f9fa" />
         </button>
       </div>
