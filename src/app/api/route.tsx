1b325292ff561d9e95163783de7a4e9a11453bd0
dev-rome <haynesjerome@gmail.com>
2023-05-24 15:15:49 -0400

    added function to add todo

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -4,7 +4,21 @@ interface TodoProps {
 }
 
 export const getTodos = async (): Promise<TodoProps[]> => {
-  const res = await fetch("http://localhost:3001/todo-list");
+  const res = await fetch("http://localhost:3001/todo-list", {
+    cache: "no-cache",
+  });
   const todos = await res.json();
   return todos;
+};
+
+export const addTodo = async (text: TodoProps): Promise<TodoProps> => {
+  const res = await fetch("http://localhost:3001/todo-list", {
+    method: "POST",
+    body: JSON.stringify(text),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const todo = await res.json();
+  return todo;
 };
